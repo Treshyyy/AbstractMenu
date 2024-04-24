29e1d7fd2892e9a9411cbf7782edd0997db7b68c
Treshyyy <116669607+Treshyyy@users.noreply.github.com>
2024-04-24 22:06:24 +0300

    Add files via upload

---
+++ AbstractMenu/src/main/java/com/cota/abstractmenu/AbstractMenu.java
@@ -0,0 +1,22 @@
+package com.cota.abstractmenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class AbstractMenu extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public void setupAbstractMenu(){
+        ;
+    }
+}
